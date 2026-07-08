ea34d627d3d7b139683442414b936c457cdefc6c
Manny <josee8090@gmail.com>
2026-07-08 02:38:15 -0400

    got multiple characters in a random assortment to fall down the screen

--- test.py
+++ test.py
@@ -8,16 +8,14 @@ def rain_test(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(True)
 
-    # row = 0
-    # col = []
-
-    # height, width = stdscr.getmaxyx()
 
     current_row = 0
     col = []
+
+
     height, width = stdscr.getmaxyx()
     
-    
+    #sets a random row value to any new number drawn on the screen(it just makes the number go in different places)
     for i in range(width):
         col.append(random.randint(-height, -1)) 
 
@@ -25,17 +23,22 @@ def rain_test(stdscr):
         
         stdscr.clear()
 
+
         for i, current_row in enumerate(col):
            
-           if current_row >= 0 and current_row < 0:
-               stdscr.addstr(current_row, i, "1")
-               col[i] += 1
+           if current_row >= 0 and current_row < height:
+                
+                #tells the program to do nothing when numbers are drawn below the height of the terminal 
+                try:
+                    stdscr.addstr(current_row, i, "1")  
+                except curses.error:
+                    pass
+                 
+           col[i] += 1 
+
 
            if col[i] > height-1: 
                 col[i] = 0
-
-
-           
 
         stdscr.refresh()
         time.sleep(0.1)
