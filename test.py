efdc760aa0f3de4d855b62cdd9f98145a262c743
Manny <josee8090@gmail.com>
2026-07-09 12:54:45 -0400

    FINNALLY got the characters to go at different speed, im currently trying to implement a strip of chnaging characters(1,0)

--- test.py
+++ test.py
@@ -7,38 +7,44 @@ def rain_test(stdscr):
 
     curses.curs_set(0)
     stdscr.nodelay(True)
-
-
+    
     current_row = 0
     col = []
-
+    speed = []
+    frame_ctr = 0
+    char = ["1", "0"]
 
     height, width = stdscr.getmaxyx()
     
-    #sets a random row value to any new number drawn on the screen(it just makes the number go in different places)
+    #fills our col list with random values (how we get the different character positions)
     for i in range(width):
         col.append(random.randint(-height, -1)) 
 
+    for j in range(width):
+        speed.append(random.randint(1, 3))
+
+
     while True:
-        
         stdscr.clear()
 
+        frame_ctr += 1
 
         for i, current_row in enumerate(col):
-           
+
            if current_row >= 0 and current_row < height:
                 
                 #tells the program to do nothing when numbers are drawn below the height of the terminal 
                 try:
-                    stdscr.addstr(current_row, i, "1")  
+                    stdscr.addstr(current_row, i, random.choice(char), curses.COLOR_GREEN)  
                 except curses.error:
                     pass
-                 
-           col[i] += 1 
+
+           if frame_ctr % speed[i] == 0:
+                col[i] += 1
 
 
            if col[i] > height-1: 
-                col[i] = 0
+                col[i] = random.randint(-height, -1)
 
         stdscr.refresh()
         time.sleep(0.1)
